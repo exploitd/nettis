9fcd2f12091b4d6d93128d014b07ba85eca21a88
Halis Duraki <duraki@null.net>
2017-11-25 01:34:53 +0100

    add: extenumus

--- src/nettis/meta.cr
+++ src/nettis/meta.cr
@@ -1,6 +1,15 @@
 module Nettis
   module Meta 
 
+    ZONE_EXTENSION = [
+      ".ba",      # => Default
+      ".org.ba",  # => Organizations
+      ".net.ba",  # => Network
+      ".gov.ba",  # => Government
+      ".edu.ba",  # => Educational
+      ".mil.ba",  # => Military (rare but exists, not visible in stats due to state protection)
+    ]
+
     def self.p(str)
       print "#{str}\n"
     end
