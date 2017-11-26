aff5cb1deba684d5372099c5145bd670a5edc18e
Halis Duraki <duraki@null.net>
2017-11-26 18:57:03 +0100

    fix: map ext to type

--- src/nettis/meta.cr
+++ src/nettis/meta.cr
@@ -1,17 +1,22 @@
 module Nettis
   module Meta 
 
-    ZONE_EXTENSION = [
-      ".ba",      # => Default
-      ".org.ba",  # => Organizations
-      ".net.ba",  # => Network
-      ".gov.ba",  # => Government
-      ".edu.ba",  # => Educational
-      ".mil.ba",  # => Military (rare but exists, not visible in stats due to state protection)
-    ]
+    ZONE_EXTENSION = {
+      ".ba"     => 1,  # => Default
+      ".org.ba" => 2,  # => Organizations
+      ".net.ba" => 3,  # => Network
+      ".gov.ba" => 4,  # => Government
+      ".mil.ba" => 5,  # => Military (rare but exists)
+      ".edu.ba" => 6,  # => Educational
+    }
 
     def self.p(str)
       print "#{str}\n"
+    end
+
+    # Parse domain to type for NIC usage
+    def self.parse_domain_to_whois(domain)
+        
     end
 
   end
