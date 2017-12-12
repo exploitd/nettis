6d6e642bd351572a738e0f83f1e9037e7d6bb43d
Halis Duraki <duraki@null.net>
2017-12-12 22:58:05 +0100

    add: tags/hotwords

---
+++ src/nettis/record/parse/nic/tags.cr
@@ -0,0 +1,20 @@
+module Nettis
+
+  class Nic
+    class TAGS
+
+      SIDEBAR = "right_text_normal_td" 
+
+    end
+
+    class HOTWORDS
+      
+      WHOIS_ADMINISTRATION = "Administrativni"
+      WHOIS_USER           = "Korisnik"
+
+    end
+
+    
+  end
+
+end
